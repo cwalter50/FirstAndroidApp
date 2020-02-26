5470faee5f33ba876ebddb37f9df8b81318ef76c
Chris Walter <walt50@gmail.com>
2020-02-25 21:49:00 -0500

    FirstFragment works

--- app/src/main/java/com/docsapps/firstandroidapp/SecondFragment.java
+++ app/src/main/java/com/docsapps/firstandroidapp/SecondFragment.java
@@ -4,18 +4,31 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import java.util.Random;
+
 public class SecondFragment extends Fragment {
+
 
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
+        Integer count = 20;
+        Random random = new java.util.Random();
+        Integer randomNumber = 0;
+        if (count > 0) {
+            randomNumber = random.nextInt(count + 1);
+        }
+//        TextView randomView = view.getRootView().findViewById(R.id.textview_random);
+//        TextView randomView = fragmentSecondLayout.findViewById(R.id.textview_first);
+//        randomView.setText(randomNumber.toString());
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_second, container, false);
     }
